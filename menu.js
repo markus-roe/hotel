ae3ad79eea5a86a0aed27bf800529f81ff4c11ef
Maximilian Sinnl <max.sinnl@hotmail.com>
2022-09-21 14:58:01 +0200

    furz2

--- menu.js
+++ menu.js
@@ -1,24 +1,30 @@
 
 function Menu() {
-    this.hamburger = document.querySelector("#hamburger")
-    this.menu = document.querySelector(".navbar-mobile")
+    this.hamburger = document.querySelector(".hamburger")
+    this.menu = document.querySelector("#nav-links")
     this.menuIsOpen = false;
 }
 function open() {
-    this.menu.style.display = "block";
+    this.menu.classList.remove("closed");
+    this.hamburger.classList.add("closed");
     this.menuIsOpen = true;
 }
 
 function close() {
-    this.menu.style.display = "none";
     this.menuIsOpen = false;
+    this.hamburger.classList.remove("closed");
+    this.menu.classList.add("closed");
 }
 
+function toggle() {
+    this.menuIsOpen ? this.close() : this.open();
+
+}
 
 Menu.prototype.open = open;
 Menu.prototype.close = close;
-Menu.prototype.toggle = function() {
-    this.menuIsOpen ? this.close() : this.open();
-}
+Menu.prototype.toggle = toggle;
+const menu = new Menu();
+document.querySelector(".hamburger").addEventListener("click", () => {menu.toggle()})
 
-
+//TODO: wenn man neben das Menu klickt, sollte es sich schließen
