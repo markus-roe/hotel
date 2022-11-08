e5e11f339713c27816fdffcafc720502b534b52f
Maximilian Sinnl <max.sinnl@hotmail.com>
2022-11-08 19:49:39 +0100

    Menu in Bootstrap 5.2.2 implementiert, Ordnerstruktur fuer bessere uebersicht angepasst

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php
 require_once "./templates/header.php";
-require_once "./templates/menu.php";
+require_once "./templates/newMenuLarge.php";
 require_once "./templates/content.php";
 
 require_once "./templates/footer.php";
--- templates/content.php
+++
@@ -1,1 +0,0 @@
-<!-- Bootstrap Content-Wrapper opening tags -->
