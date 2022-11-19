ed761d210253d378fc781c911d6c67295f765540
mustermaxs <max.sinnl@hotmail.com>
2022-11-19 14:32:23 +0100

    ipsum-textblock, contentWrapperStart style

--- public/templates/contentWrapperStart.php
+++ public/templates/contentWrapperStart.php
@@ -1,7 +1,8 @@
-<div class="container">
+<div class="container" id="ipsum-content-wrapper">
     <div class="title-section mt-3 mb-3 p-0 text-center">
         <h1>{{page-title}}</h1>
     </div>
     <div class="row">
         <div class="col-md-2 col-xs-1"></div>
-        <div class="col-xs-12 col-md-8 ipsum-centered">
+        <div class="col-xs-12 col-md-8">
+        <!-- <div class="col-xs-12 col-md-8 ipsum-centered"> -->
