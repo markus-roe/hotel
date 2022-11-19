30bae4cf1a98c5b4d0fad38ed04780cf4a0c3f20
Markus Rösner <markus.roesner@gmx.net>
2022-11-19 22:53:36 +0100

    added base model

---
+++ core/model.php
@@ -0,0 +1,33 @@
+<?php
+require_once  getcwd()."/config.php";
+
+class Model
+{
+
+    public $connection;
+
+    public function __construct() 
+    {
+        $this->connection = $this->connect();
+    } 
+
+    public function connect()
+    {
+        try{
+            $config = new Config();
+            $mysqli = new mysqli($config->host, $config->user, $config->password, $config->database);
+
+            if ($mysqli -> connect_errno) {
+                echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+                exit();
+            }
+
+        }
+        catch(Exception $e) 
+        {
+            echo 'Message: ' .$e->getMessage();
+        }
+
+        return $mysqli;
+    }
+}
