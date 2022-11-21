ffde723b45828fda1b58d05675f1af57d9e981fb
mustermaxs <max.sinnl@hotmail.com>
2022-11-21 13:31:57 +0100

    created article-template

---
+++ public/templates/articleTemplate.php
@@ -0,0 +1,39 @@
+<!-- CONTENT_START -->
+<article class="ipsum-article horizontalLine">
+            <h2>Hubsi wird neuer Hoteldirektor</h2>
+            <p>
+              <h6>Christian Hubsi tritt im nächsten Frühjahr vorraussichtlich Stelle
+                als neuer Hoteldirektor an.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Facere vel assumenda accusamus possimus quas quasi nemo debitis dolorum,
+                totam mollitia iste odit amet nesciunt dolor dignissimos eius ipsa aut ullam?
+              </h6>
+            </p>
+            <div class="text-center container-fluid">
+            <img src="https://picsum.photos/1100/500" class="rounded img-fluid" alt="...">
+          </div>
+
+        <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure illo ipsa quam id impedit
+          nisi eaque praesentium nam eligendi fuga sequi voluptas velit, consequuntur dolorum.</p>
+        <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda minima deleniti iure
+          voluptatum aperiam veritatis quae delectus nulla, nisi beatae tempora voluptatibus cumque ex in vero aut,
+          dolor nam saepe. Sunt quisquam voluptatibus iure ipsa. Labore, accusamus quod neque iusto corrupti natus
+          tempora sed ipsum quas unde deserunt esse eaque! Et rem non earum ut, quis quaerat labore ducimus recusandae
+          sint molestiae vitae iste impedit blanditiis magni aliquam. Autem id consequatur nostrum. Incidunt deserunt
+          minus debitis sequi minima voluptas, vel animi delectus omnis aut ex nulla ad doloremque suscipit consequatur
+          quasi ea fugit laudantium mollitia officia illum? Sint, unde ratione.</p>
+        <h3>Ipsum - Dolor sit amet</h3>
+
+        <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda minima deleniti iure
+          voluptatum aperiam veritatis quae delectus nulla, nisi beatae tempora voluptatibus cumque ex in vero aut,
+          dolor nam saepe. Sunt quisquam voluptatibus iure ipsa. Labore, accusamus quod neque iusto corrupti natus
+          tempora sed ipsum quas unde deserunt esse eaque! Et rem non earum ut, quis quaerat labore ducimus recusandae
+          sint molestiae vitae iste impedit blanditiis magni aliquam. Autem id consequatur nostrum. Incidunt deserunt
+          minus debitis sequi minima voluptas, vel animi delectus omnis aut ex nulla ad doloremque suscipit consequatur
+          quasi ea fugit laudantium mollitia officia illum? Sint, unde ratione.</p>
+        <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vitae quos deserunt quae
+          corporis quidem ad iure! Vel sed velit voluptas facere numquam non itaque.</p>
+
+
+        </article>
+            <!-- CONTENT_END -->
