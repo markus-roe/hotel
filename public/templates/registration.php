b4e47726a3655559e6e85e6df4ef8883c918c7cc
mustermaxs <max.sinnl@hotmail.com>
2022-11-21 11:27:51 +0100

    created registration content-templatem

---
+++ public/templates/registration.php
@@ -0,0 +1,48 @@
+<div class="ipsum-form">
+    <form action="" method="POST">
+        <div class="ipsum-fieldset">
+            <div class="ipsum-input-wrapper">
+                <div class="ipsum-input-container horizontalLine">
+
+                <div class="col first">
+                   <h5>Gender</h5>
+                </div>
+                <div class="col">
+                    <input class="ipsum-input" type="radio" name="inlineRadioOptions" id="female"
+                    value="female" />
+                  <label  for="female">Female</label>
+                </div>
+                <div class="col">
+                    <input class="ipsum-input" type="radio" name="inlineRadioOptions" id="male"
+                    value="male" />
+                  <label for="male">Male</label>
+                </div>
+                <div class="col">
+                    <input class="ipsum-input" type="radio" name="inlineRadioOptions" id="other"
+                    value="other" />
+                  <label for="other">Other</label>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="ipsum-input-container labelOnTop horizontalLine">
+            <label for="fname">First Name</label>
+            <input class="ipsum-input text" type="text" id="fname" name="fname" placeholder="First name"/>                            
+            <label for="lname">Last Name</label>
+            <input class="ipsum-input text" type="text" id="lname" name="lname" placeholder="Last name"/>                            
+                </div>
+
+            <div class="ipsum-input-container labelOnTop">
+                <label for="username">Username</label>
+                <input class="ipsum-input text" type="text" id="username" name="username" placeholder="Username"/>                            
+            </div>
+            <div class="ipsum-input-container labelOnTop">
+                <label for="password">Password</label>
+                <input type="password" id="password" class="ipsum-input text" name="password" placeholder="Password">
+            </div>
+            <div class="ipsum-input-container labelOnTop">
+                <label for="password2">Repeat Password</label>
+                <input type="password" class="ipsum-input text" name="password2" id="password2" placeholder="Repeat Password">
+
+            </div>
+        </div>
