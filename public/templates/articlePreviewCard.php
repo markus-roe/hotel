1250e4ed5929344108ca3a56073f6dad3bc57b65
mustermaxs <max.sinnl@hotmail.com>
2022-11-21 10:23:40 +0100

    implemented article preview templates

---
+++ public/templates/articlePreviewCard.php
@@ -0,0 +1,21 @@
+<div class="col-sm-6">
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="https://placekitten.com/300/300" class="img-fluid rounded" alt="..." />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><a class="link" href="{{article-link}}" style="text-decoration: none">{{headline}}</a></h5>
+                    <p class="card-text">
+                        {{preview}}
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated on {{updated}}</small><br>
+                    </p>
+                    <button type="button" class="btn btn-primary btn-sm"><a href="{{article-link}}" style="color:white; text-decoration: none">Read more</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ public/templates/articleRow.php
@@ -0,0 +1,4 @@
+<div class="row">
+    {{prev-card-1}}
+    {{prev-card-2}}
+</div>
