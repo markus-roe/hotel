712521e51b3f35517067c08670d9ff90f1d555dc
mustermaxs <max.sinnl@hotmail.com>
2022-11-22 12:26:00 +0100

    updated faq-page template for bootstrap 5.2

---
+++ helppage.php
@@ -0,0 +1,20 @@
+<?php
+require_once "./templates_new/header.php";
+require_once "./templates_new/menu.php"; ?>
+
+<div class="container">
+    <div class="title-section mt-3 mb-3 p-0 text-center">
+        <h1>Help & FAQ</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-1"></div>
+        <div class="col-xs-12 col-md-8">
+<!-- CONTENT_START -->
+<?php include "./public/templates/faqTemplate.php";?>
+            <!-- CONTENT_END -->
+            <div class="col-md-2 col-xs-1"></div>
+
+            <footer></footer>
+            </body>
+
+            </html>
