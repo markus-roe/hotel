d10bacf5803040f55aedd679cf023875c8f5bd92
mustermaxs <max.sinnl@hotmail.com>
2022-11-24 18:04:52 +0100

    implemented profile-content, profile links templates

---
+++ core/config.php
@@ -0,0 +1,11 @@
+<?php
+class config
+{
+    function __construct() {
+        $this->host = "localhost";
+        $this->user  = "root";
+        $this->password = "Wlenfeni1428780";
+        $this->database = "ipsum";
+    }
+}
+?>
---
+++ profile_1.php
@@ -0,0 +1,32 @@
+<?php
+require_once "./templates_new/header.php";
+require_once "./templates_new/menu.php"; ?>
+
+<div class="container">
+    <div class="title-section mt-3 mb-3 p-0 text-center">
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-1"></div>
+        <div class="col-xs-12 col-md-8">
+            <!-- CONTENT_START -->
+            <div class="text-center  horizontalLine">
+                <h1>Profile</h1>
+            </div>
+
+            <div class="text-center">
+                <h5><a href="" style="text-decoration: none; color:black; line-height: 3rem">Stammdaten</a></h5>
+                <h5><a href="" style="text-decoration: none; color:black; line-height: 3rem">Buchungen</a></h5>
+                <h5><a href="" style="text-decoration: none; color:black; line-height: 3rem">Neuer News-Artikel</a></h5>
+            </div>
+
+
+            <!-- CONTENT_END -->
+        </div>
+    </div>
+</div>
+<div class="col-md-2 col-xs-1"></div>
+</div>
+</div>
+</body>
+
+</html>
