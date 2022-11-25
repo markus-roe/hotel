0e0e7af66d63f4c01c6fc6e28bf8cab9e95b8169
mustermaxs <max.sinnl@hotmail.com>
2022-11-25 17:45:41 +0100

    stable

--- core/controllers/registrationController.php
+++ core/controllers/registrationController.php
@@ -13,11 +13,25 @@ class RegistrationController extends Controller
             case "newuser":
                 $this->renderRegistrationPage();
                 break;
+            case "missing":
+                $this->renderMissingInputPage();
+                break;
+            case "passwordnotmatching":
+                $this->renderPasswordNotMatchingPage();
+                break;
             default:
                 $this->renderErrorPage();
                 break;
         }
 
+    }
+
+    private function renderPasswordNotMatchingPage()
+    {
+        $this->getView("/Pages/registrationPage");
+        $page = new RegistrationPage();
+        $page->parse(["cssinputclass"=>"inputError", "errormsg"=>"Passwords not matching!"]);
+        $page->render();
     }
 
     private function renderRegistrationPage()
@@ -28,11 +42,57 @@ class RegistrationController extends Controller
         $page->render();
     }
 
-    public function register()
+    private function renderMissingInputPage()
     {
-        $result = $this->clientMode->registerNewUser();
+        $this->getView("/Pages/registrationPage");
+        $page = new RegistrationPage();
+        $page->parse(["cssinputclass"=>"inputError", "errormsg"=>"Inputs missing!"]);
+        $page->render();
+    }
 
-        // if ($result == ErrorCode::)
+    public function registerAction()
+    {
+        /*
+            return ErrorCode::MISSING_INPUT;
+            return ErrorCode::PASSWORD_TOO_LONG;
+
+        */
+        if (
+            !isset($_POST["gender"]) ||
+            !isset($_POST["firstname"]) ||
+            !isset($_POST["surname"]) ||
+            !isset($_POST["username"]) ||
+            !isset($_POST["password"]) ||
+            !isset($_POST["email"]) ||
+            !isset($_POST["password2"]))
+            {
+                header("Location: ../registration/missing/index");
+                return false;
+            }
+    
+            if ($_POST["password"] != $_POST["password2"])
+            {
+                header("Location: ../registration/passwordnotmatching/index");
+                return false;
+            }
+    
+            
+
+        $registrationSuccessfull = $this->clientModel->registerNewUser(
+            $_POST["firstname"],
+            $_POST["surname"],
+            $_POST["username"],
+            $_POST["password"],
+            $_POST["gender"],
+            $_POST["email"]
+        );
+
+        if ($registrationSuccessfull)
+        {
+            $this->clientModel->loginUser($_POST["username"], $_POST["password"]);
+            header("Location: ../profile/".$_SESSION["rolename"]."/index");
+
+        }
 
     }
 }
