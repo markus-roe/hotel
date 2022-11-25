8d092b1274e7a1ea93106a686d800e0d3162b22e
Markus Rösner <markus.roesner@gmx.net>
2022-11-25 18:56:09 +0100

    personal profile page

--- public/views/Pages/profilePage.php
+++ public/views/Pages/profilePage.php
@@ -1,31 +1,22 @@
 <?php
 
 require_once getcwd()."/public/views/Components/page.php";
+require_once getcwd()."/core/user.php";
 
 class ProfilePage extends Page
 {
     function __construct()
     {
         parent::__construct();
-        
-        $profileLinks = [];
-        $profileLinksHtml = $this->createProfileLinks($profileLinks);
-        $content = new View("profileContentTemplate", ["profile-links"]);
+
+
+        $content = new View("personalDataTemplate", ["page-title" => "Stammdaten", "profile-update-link" => "test", "firstname", "surname", "username", "email", "phone"]);
+        // $content->parse();
+        $this->changeContent($content);
         
     }
 
-    protected function createProfileLinks($profileLinks)
-    {
-        $htmlString = "";
-        $linkTemplate = View::readFromFile(getcwd()."/public/templates/"."profileLinkTemplate");
 
-        foreach($profileLinks as $link)
-        {
-            $htmlString .= View::parseTemplate($linkTemplate, ["profile-link-href" => $link["profile-link-href"], "profile-link-text" => $link["profile-link-text"]]);
-        }
-
-        return $htmlString;
-    }
 }
 //  /profile/menu/
 //  /profile/stammdaten
