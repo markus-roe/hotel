5e069616f940a8711a89047fa7ce642bb2f7b7c3
mustermaxs <max.sinnl@hotmail.com>
2022-12-06 14:43:33 +0100

    inserted slots, fixed styling issue

---
+++ public/templates/bookingCardTemplate.php
@@ -0,0 +1,65 @@
+<div class="ipsum-card">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Name
+                                </span>
+                                <a href="">{{firstname}} {{surname}}</a>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Zeitraum
+                                </span>
+                                <textarea rows="1" cols="10" name="startDate" class="ipsum-card-input">{{startdate}}</textarea>
+                                <textarea rows="1" cols="10" name="endDate" class="ipsum-card-input">{{endDate}}</textarea>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Status
+                                </span>
+                                <select class="ipsum-card-input select-option" data-labeltype="confirmed" name="" id="">
+                                    <option data-labeltype="confirmed" value="confirmed" {{status-confirmed}}>Bestätigt</option>
+                                    <option data-labeltype="new" value="new" {{status-new}}>Neu</option>
+                                    <option data-labeltype="storno" value="storno" {{status-storno}}>Storniert</option>
+                                </select>
+                            </div>
+                        </div>
+
+                    </div>
+                        <!-- <div class="w-100"></div> -->
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Services
+                                </span>
+                            <ul class="services-list">
+                                {{services}}
+                            </ul>
+
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Gesamtpreis
+                                </span>
+                                <span>€</span>
+                                <input style="width:5rem" class="ipsum-card-input" type="number" min="0.00" step="0.01" name="roomId" id="" value="{{price}}">
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">Zimmernr.</span>
+                                <input class="ipsum-card-input" type="text" name="roomId" id="" value="{{roomId}}">
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
