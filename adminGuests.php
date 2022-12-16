8a99c71001ec19737214d440599c36388c207b1a
mustermaxs <max.sinnl@hotmail.com>
2022-12-16 20:58:22 +0100

    implemented guestCard

---
+++ adminGuests.php
@@ -0,0 +1,91 @@
+<?php include "templates_new\header.php";
+include "templates_new\menu.php" ?>
+<div class="container">
+    <div class="title-section mt-3 mb-3 p-0 text-center">
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-1"></div>
+        <div class="col-xs-12 col-md-8">
+            <div class="ipsum-card" style="height: fit-content !important;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Vorname
+                                </span>
+                                <a href="">Max</a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Nachname
+                                </span>
+                                <a href="">Mustermann</a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Geschlecht
+                                </span>
+                                <span>männlich</span>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Username
+                                </span>
+                                <span>msinnl</span>
+                            </div>
+                        </div>
+                        <!-- <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Personen
+                                </span>
+                                <input type="text" rows="1" name="" class="ipsum-card-input" value="4">
+                            </div>
+                        </div> -->
+
+
+                    </div>
+                    <!-- <div class="w-100"></div> -->
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Email
+                                </span>
+                                <span>max.sinnl@hotmail.com</span>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Telefonnummer
+                                </span>
+                                <span>06607707635</span>
+                                
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell" style="display: flex; flex-direction:column">
+                                <button type="button" class="btn btn-danger">Löschen</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-2 col-xs-1"></div>
+</div>
+</div>
+</body>
+
+</html>
+<!-- CONTENT END -->
---
+++ public/templates/guestCardTemplate.php
@@ -0,0 +1,62 @@
+<div class="ipsum-card" style="height: fit-content !important;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Vorname
+                                </span>
+                                <a href="">Max</a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Nachname
+                                </span>
+                                <a href="">Mustermann</a>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Geschlecht
+                                </span>
+                                <span>männlich</span>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cell">
+                                <span class="title">
+                                    Username
+                                </span>
+                                <span>msinnl</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Email
+                                </span>
+                                <span>max.sinnl@hotmail.com</span>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell">
+                                <span class="title">
+                                    Telefonnummer
+                                </span>
+                                <span>06607707635</span>
+                                
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="cell" style="display: flex; flex-direction:column">
+                                <button type="button" class="btn btn-danger">Löschen</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
