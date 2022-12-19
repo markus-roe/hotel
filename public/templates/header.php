3837af4d1fbbbb04bb919fb6cb1dcea0b2462222
Markus Rösner <markus.roesner@gmx.net>
2022-12-19 22:58:30 +0100

    garbage collector #2

--- public/templates/header.php
+++ public/templates/header.php
@@ -9,8 +9,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" type="text/css" href="./public/css/fonts.css">
-  <!-- <link rel="stylesheet" type="text/css" href="./css/style.css"> -->
-  <link rel="stylesheet" type="text/css" href="./public/css/styleNew.css">
   <link rel="stylesheet" type="text/css" href="./public/css/newMenu.css">
   <link rel="stylesheet" type="text/css" href="./public/css/newMenuMobile.css">
   <link rel="stylesheet" type="text/css" href="./public/css/ipsum_form.css">
