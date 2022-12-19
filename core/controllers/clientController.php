fdc4c1b794448cc00bc99a32f4ce5508016dc9ad
mustermaxs <max.sinnl@hotmail.com>
2022-12-19 21:29:30 +0100

    error message when pwds not matching

--- core/controllers/clientController.php
+++ core/controllers/clientController.php
@@ -67,6 +67,14 @@ class ClientController extends Controller
         $post_phone = $_POST['phone'] ? $_POST['phone'] : "";
         $post_password = $_POST['new-password'] ? $_POST['new-password'] : "";
         $post_confirmpassword = $_POST['confirm-new-password'] ? $_POST['confirm-new-password'] : "";
+
+        if ($post_confirmpassword != $post_password)
+        {
+            header("Location: ../client/profile?res=passworderr");
+
+            return 0;
+        }
+
         $this->clientModel->updateUserData($post_firstname, $post_surname, $post_email, $post_phone, $post_password, $post_confirmpassword, $userId);
         
         header("Location: ../client/profile?res=updatesuccess");
@@ -75,10 +83,16 @@ class ClientController extends Controller
     public function renderProfilePage()
     {
         $this->profilePage = new $this->pageName();
-        if ($this->request["res"] == "updatesuccess")
+        switch ($this->request["res"])
         {
-            $this->profilePage->triggerPopup("<span style='font-size:1.5rem'>🥳</span> Update erfolgreich!");
+            case "updatesuccess":
+                $this->profilePage->triggerPopup("<span style='font-size:1.5rem'>🥳</span> Update erfolgreich!");
+                break;
+            case "passworderr":
+                $this->profilePage->triggerPopup("<span style='font-size:1.5rem'>😔</span> Passwörter stimmen nicht überein!");
+                break;
         }
+
         $this->profilePage->parse([...$this->userData, $this->request["current_uri"]]);
         $this->profilePage->render();
     }
